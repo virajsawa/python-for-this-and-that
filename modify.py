749504a4275532602b08755f5094d7d80043bcd8
viraj <virajsawa@gmail.com>
2013-10-14 19:10:33 +0530

    Script replicated. Prior testing commit

--- modify.py
+++ modify.py
@@ -16,3 +16,42 @@ try:
     print "I/O error({0}): {1}".format(e.errno, e.strerror)   
 except IOError as e:
   print "I/O error({0}): {1}".format(e.errno, e.strerror)
+
+
+#modifying module1_guess.m
+try:
+  stre = open('module1_guess.m', 'r');
+  stre.seek(0);
+  data = stre.readlines();
+  stre.close();
+  data[470]='';
+  data[2573]='';
+  try:
+    writ = open('module1_guess.m', 'w');
+    for lin in data:
+     writ.write("%s\n" % lin);
+    writ.close();
+  except IOError as e:
+    print "I/O error({0}): {1}".format(e.errno, e.strerror)   
+except IOError as e:
+  print "I/O error({0}): {1}".format(e.errno, e.strerror)
+
+
+#modifying module1.m
+try:
+  stre = open('module1.m', 'r');
+  stre.seek(0);
+  data = stre.readlines();
+  stre.close();
+  data[2162]='';
+  try:
+    writ = open('module1.m', 'w');
+    for lin in data:
+     writ.write("%s\n" % lin);
+    writ.close();
+  except IOError as e:
+    print "I/O error({0}): {1}".format(e.errno, e.strerror)   
+except IOError as e:
+  print "I/O error({0}): {1}".format(e.errno, e.strerror)
+
+print("files modified");
