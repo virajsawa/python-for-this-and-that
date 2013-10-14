c77fcc73b422da6cdb9ee08c46ee701ee5a6a429
viraj <virajsawa@gmail.com>
2013-10-14 19:22:24 +0530

    Tested and debugged. Fully functional.

--- modify.py
+++ modify.py
@@ -6,11 +6,11 @@ try:
   stre.seek(0);
   data = stre.readlines();
   stre.close();
-  data[2067]='';
+  data[2067]='\n';
   try:
     writ = open('module1_fun.m', 'w');
     for lin in data:
-     writ.write("%s\n" % lin);
+     writ.write("%s" % lin);
     writ.close();
   except IOError as e:
     print "I/O error({0}): {1}".format(e.errno, e.strerror)   
@@ -24,12 +24,12 @@ try:
   stre.seek(0);
   data = stre.readlines();
   stre.close();
-  data[470]='';
-  data[2573]='';
+  data[470]='\n';
+  data[2573]='\n';
   try:
     writ = open('module1_guess.m', 'w');
     for lin in data:
-     writ.write("%s\n" % lin);
+     writ.write("%s" % lin);
     writ.close();
   except IOError as e:
     print "I/O error({0}): {1}".format(e.errno, e.strerror)   
@@ -43,11 +43,11 @@ try:
   stre.seek(0);
   data = stre.readlines();
   stre.close();
-  data[2162]='';
+  data[2162]='\n';
   try:
     writ = open('module1.m', 'w');
     for lin in data:
-     writ.write("%s\n" % lin);
+     writ.write("%s" % lin);
     writ.close();
   except IOError as e:
     print "I/O error({0}): {1}".format(e.errno, e.strerror)   
