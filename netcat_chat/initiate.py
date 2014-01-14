afe330282fa319b701f8538b2f648a0234a63b74
viraj <virajsawa@gmail.com>
2014-01-14 16:48:02 +0530

    Tested. Successful. Some redundancies to be built.

--- netcat_chat/initiate.py
+++ netcat_chat/initiate.py
@@ -5,25 +5,31 @@ import time
 #----------------------------- Function Definitions
 
 def chat():
- msg = str(os.system("nc -l 55555 > buffer; cat buffer"))
- print("Guest : "+msg+"\n")
+ os.system("nc -l 55555 > buffer")
+ rd = open('buffer', 'r')
+ msg = str(rd.read()) 
+ print("Guest : "+msg)
  mymsg = raw_input("Me    : ")
  if(mymsg == "quit"):
   quit_func();
  writ = open('buffer','w')
  writ.write(mymsg)
  writ.close()
- while(~os.system("nc -z "+ip+" 55555")):
-  print("Connecting to Guest ... ")
-  time.sleep(2)
- os.system("nc "+ip+" 55555 < buffer");
- chat();
+# while(~os.system("nc -z "+ip+" 55555")):
+#  print("Connecting to Guest ... ")
+#  time.sleep(2)
+ os.system("nc "+ip+" 55555 < buffer")
+ chat()
 
 def quit_func():
  re = raw_input("Are you sure you want to close this connection ? (y/n) : ")
  if(re=='n'):
   pass;
  else:
+  writ = open('buffer','w')
+  writ.write("Guest has logged out.")
+  writ.close()
+  os.system("nc "+ip+" 55555 < buffer")
   sys.exit("Connection closed\n")
 
 #----------------------------------------------------------------------------
@@ -43,9 +49,9 @@ elif(reply == "y"):
  writ = open('buffer','w')
  writ.write(mymsg)
  writ.close()
- while(~os.system("nc -z "+ip+" 55555")):
-  print("Connecting to Guest ... ")
-  time.sleep(2)
+# while(~os.system("nc -z "+ip+" 55555")):
+#  print("Connecting to Guest ... ")
+#  time.sleep(2)
  os.system("nc "+ip+" 55555 < buffer");
  chat();
 else :
